823ba3cf8b31074ddc2d83a0586b1ff7ed491ce3
unknown <345753581@qq.com>
2021-07-06 17:53:44 +0800

    first commit

---
+++ src/pages/home/home.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import Aries  from '../../assets/image/1.png';
+import './index.scss'
+
+export default class home extends Component {
+    render() {
+        return (
+            <div className='header'>
+                <img src={Aries} alt="es-lint want to get"></img>
+                <img src={Aries} alt="es-lint want to get"></img>
+                <img src={Aries} alt="es-lint want to get"></img>
+                <img src={Aries} alt="es-lint want to get"></img>
+                <img src={Aries} alt="es-lint want to get"></img>
+                <img src={Aries} alt="es-lint want to get"></img>
+                <img src={Aries} alt="es-lint want to get"></img>
+                <img src={Aries} alt="es-lint want to get"></img>
+                <img src={Aries} alt="es-lint want to get"></img>
+
+            </div>
+        )
+    }
+}
